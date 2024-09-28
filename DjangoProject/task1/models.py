82ef103ce87ee7e033a2d45fd21ca6fadbeda2d9
GorbunovGHub <pavlixrus@mail.ru>
2024-09-28 23:59:51 +0300

    28/09/24-23:59

--- DjangoProject/task1/models.py
+++ DjangoProject/task1/models.py
@@ -12,8 +12,8 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(decimal_places=2, max_digits=10)
-    size = models.DecimalField(decimal_places=2, max_digits=10)
+    cost = models.DecimalField(decimal_places=2, max_digits=100)
+    size = models.DecimalField(decimal_places=2, max_digits=100)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='buyers')
